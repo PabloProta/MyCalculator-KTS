5003e8a512925180d85f907b115486c726af3748
Pablo Antonini Prota-PPROTA <pprota@motorola.com>
2023-04-03 22:03:13 -0300

    change app module to application instead library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,43 @@
 plugins {
-    id("base-android-library")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.example.mycalculator"
 
     defaultConfig {
+        applicationId = "com.example.mycalculator"
         compileSdk = 33
         minSdk = 29
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileSdk = 33
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    packagingOptions {
+        resources.excludes.apply {
+            add("META-INF/AL2.0")
+            add("META-INF/LGPL2.1")
+        }
     }
 
     composeOptions {
